4091421a99f533f8432396984be616cf5787149b
Michael Weirauch <michael.weirauch@gmail.com>
2020-04-16 00:13:27 +0200

    dropbox: coverage
    We can't use tmpfs on linux for testing, as it doesn't support extended
    file system attributes.

--- src/dropbox.rs
+++ src/dropbox.rs
@@ -34,3 +34,119 @@ impl Dropbox {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Dropbox;
+    use directories::BaseDirs;
+    use rstest::rstest;
+    use std::fs;
+    use std::path::{Path, PathBuf};
+    use tempfile::{Builder, NamedTempFile, TempDir};
+
+    const TEST_ITEM_PREFIX: &str = "dropignore-testing";
+    const TEST_IGNORE_ATTRIBUTE_KEY: &str = "user.com.dropbox.ignored";
+    const TEST_IGNORE_ATTRIBUTE_VALUE_IGNORED: [u8; 1] = [b'1'];
+
+    #[rstest(arrange_ignored, case::not_ignored(false), case::ignored(true))]
+    fn is_ignored_directory(arrange_ignored: bool) {
+        let temp = arrange_test_directory();
+        let path = temp.path();
+
+        if arrange_ignored {
+            arrange_ignored_attribute(path);
+        }
+
+        let dropbox = Dropbox::new().unwrap();
+
+        assert_eq!(arrange_ignored, dropbox.is_ignored(path));
+    }
+
+    #[rstest(arrange_ignored, case::not_ignored(false), case::ignored(true))]
+    fn is_ignored_file(arrange_ignored: bool) {
+        let temp = arrange_test_file();
+        let path = temp.path();
+
+        if arrange_ignored {
+            arrange_ignored_attribute(path);
+        }
+
+        let dropbox = Dropbox::new().unwrap();
+
+        assert_eq!(arrange_ignored, dropbox.is_ignored(path));
+    }
+
+    #[test]
+    fn ignore_directory() {
+        let temp = arrange_test_directory();
+        let path = temp.path();
+
+        let dropbox = Dropbox::new().unwrap();
+
+        dropbox.ignore(path);
+
+        assert!(dropbox.is_ignored(path));
+    }
+
+    #[test]
+    fn ignore_file() {
+        let temp = arrange_test_file();
+        let path = temp.path();
+
+        let dropbox = Dropbox::new().unwrap();
+
+        dropbox.ignore(path);
+
+        assert!(dropbox.is_ignored(path));
+    }
+
+    fn arrange_test_directory() -> TempDir {
+        let mut builder = Builder::new();
+        builder.prefix(TEST_ITEM_PREFIX);
+
+        let temp = if cfg!(target_os = "linux") {
+            let cache_dir = user_cache_directory_path();
+            builder.tempdir_in(cache_dir)
+        } else {
+            builder.tempdir()
+        };
+
+        temp.unwrap()
+    }
+
+    fn arrange_test_file() -> NamedTempFile {
+        let mut builder = Builder::new();
+        builder.prefix(TEST_ITEM_PREFIX);
+
+        let temp = if cfg!(target_os = "linux") {
+            let cache_dir = user_cache_directory_path();
+            builder.tempfile_in(cache_dir)
+        } else {
+            builder.tempfile()
+        };
+
+        temp.unwrap()
+    }
+
+    #[cfg(target_os = "linux")]
+    fn user_cache_directory_path() -> PathBuf {
+        // we can't use tmpfs on linux as it doesn't
+        // support extended file system attributes
+        let base_dirs = BaseDirs::new().unwrap();
+        let cache_dir = base_dirs.cache_dir();
+
+        // create in case it's not present
+        fs::create_dir_all(cache_dir).unwrap();
+
+        cache_dir.into()
+    }
+
+    fn arrange_ignored_attribute(path: &Path) {
+        xattr::set(
+            path,
+            TEST_IGNORE_ATTRIBUTE_KEY,
+            &TEST_IGNORE_ATTRIBUTE_VALUE_IGNORED,
+        )
+        .unwrap();
+    }
+}
