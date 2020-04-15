7e61088ffa74c3c46f3ce77606b571b20ac997a1
Michael Weirauch <michael.weirauch@gmail.com>
2020-04-15 19:20:23 +0200

    dropbox: coverage

--- src/dropbox.rs
+++ src/dropbox.rs
@@ -34,3 +34,105 @@ impl Dropbox {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Dropbox;
+    use directories::BaseDirs;
+    use rstest::rstest;
+    use std::path::Path;
+    use tempfile::{Builder, NamedTempFile, TempDir};
+
+    const TEST_ITEM_PREFIX: &str = "dropignore-testing";
+    const TEST_IGNORE_ATTRIBUTE_KEY: &str = "user.com.dropbox.ignored";
+    const TEST_IGNORE_ATTRIBUTE_VALUE_IGNORED: [u8; 1] = [b'1'];
+
+    #[rstest(arrange_ignored, case::not_ignored(false), case::ignored(true))]
+    fn is_ignored_directory(arrange_ignored: bool) {
+        let temp = arrange_test_directory();
+        let path = temp.path();
+
+        if arrange_ignored {
+            arrange_ignored_attribute(path);
+        }
+
+        let dropbox = Dropbox::new().unwrap();
+
+        assert_eq!(arrange_ignored, dropbox.is_ignored(path));
+    }
+
+    #[rstest(arrange_ignored, case::not_ignored(false), case::ignored(true))]
+    fn is_ignored_file(arrange_ignored: bool) {
+        let temp = arrange_test_file();
+        let path = temp.path();
+
+        if arrange_ignored {
+            arrange_ignored_attribute(path);
+        }
+
+        let dropbox = Dropbox::new().unwrap();
+
+        assert_eq!(arrange_ignored, dropbox.is_ignored(path));
+    }
+
+    #[test]
+    fn ignore_directory() {
+        let temp = arrange_test_directory();
+        let path = temp.path();
+
+        let dropbox = Dropbox::new().unwrap();
+
+        dropbox.ignore(path);
+
+        assert!(dropbox.is_ignored(path));
+    }
+
+    #[test]
+    fn ignore_file() {
+        let temp = arrange_test_file();
+        let path = temp.path();
+
+        let dropbox = Dropbox::new().unwrap();
+
+        dropbox.ignore(path);
+
+        assert!(dropbox.is_ignored(path));
+    }
+
+    fn arrange_test_directory() -> TempDir {
+        // we can't use tmpfs - at least on linux - as that one doesn't
+        // support extended file system attributes
+        let base_dirs = BaseDirs::new().unwrap();
+        let cache_dir = base_dirs.cache_dir();
+
+        let temp = Builder::new()
+            .prefix(TEST_ITEM_PREFIX)
+            .tempdir_in(cache_dir)
+            .unwrap();
+
+        temp
+    }
+
+    fn arrange_test_file() -> NamedTempFile {
+        // we can't use tmpfs - at least on linux - as that one doesn't
+        // support extended file system attributes
+        let base_dirs = BaseDirs::new().unwrap();
+        let cache_dir = base_dirs.cache_dir();
+
+        let temp = Builder::new()
+            .prefix(TEST_ITEM_PREFIX)
+            .tempfile_in(cache_dir)
+            .unwrap();
+
+        temp
+    }
+
+    fn arrange_ignored_attribute(path: &Path) {
+        xattr::set(
+            path,
+            TEST_IGNORE_ATTRIBUTE_KEY,
+            &TEST_IGNORE_ATTRIBUTE_VALUE_IGNORED,
+        )
+        .unwrap();
+    }
+}
