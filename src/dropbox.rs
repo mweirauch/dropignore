61d04f3b51452f32e21bcc45ddba653b47da25a6
Michael Weirauch <michael.weirauch@gmail.com>
2020-05-25 21:03:44 +0200

    Windows support via winapi

--- src/dropbox.rs
+++ src/dropbox.rs
@@ -1,8 +1,26 @@
 use std::path::Path;
+#[cfg(windows)]
+use std::ptr;
 
 use log::warn;
 
+#[cfg(windows)]
+use widestring::U16CString;
+#[cfg(windows)]
+use winapi::{
+    shared::minwindef::{DWORD, FALSE, LPCVOID, LPVOID, TRUE},
+    um::{
+        errhandlingapi::GetLastError,
+        fileapi::{CreateFile2, ReadFile, WriteFile, CREATE_ALWAYS, OPEN_EXISTING},
+        handleapi::CloseHandle,
+        winnt::{FILE_SHARE_READ, FILE_SHARE_WRITE, GENERIC_READ, GENERIC_WRITE},
+    },
+};
+
+#[cfg(unix)]
 const IGNORE_ATTRIBUTE_KEY: &str = "user.com.dropbox.ignored";
+#[cfg(windows)]
+const IGNORE_ATTRIBUTE_KEY: &str = "com.dropbox.ignored";
 const IGNORE_ATTRIBUTE_VALUE_IGNORED: [u8; 1] = [b'1'];
 
 pub struct Dropbox {}
@@ -12,6 +30,7 @@ impl Dropbox {
         Ok(Dropbox {})
     }
 
+    #[cfg(unix)]
     pub fn is_ignored(&self, path: &Path) -> bool {
         match xattr::get(path, IGNORE_ATTRIBUTE_KEY) {
             Ok(attribute) => {
@@ -26,6 +45,7 @@ impl Dropbox {
         }
     }
 
+    #[cfg(unix)]
     pub fn ignore(&self, path: &Path) -> bool {
         match xattr::set(path, IGNORE_ATTRIBUTE_KEY, &IGNORE_ATTRIBUTE_VALUE_IGNORED) {
             Ok(_) => true,
@@ -34,6 +54,117 @@ impl Dropbox {
                 false
             }
         }
+    }
+
+    #[cfg(windows)]
+    pub fn is_ignored(&self, path: &Path) -> bool {
+        let attribute = xattr_get(path, IGNORE_ATTRIBUTE_KEY);
+        if let Some(bytes) = attribute {
+            if bytes.eq(&IGNORE_ATTRIBUTE_VALUE_IGNORED) {
+                return true;
+            }
+            return false;
+        }
+        false
+    }
+
+    #[cfg(windows)]
+    pub fn ignore(&self, path: &Path) -> bool {
+        xattr_set(path, IGNORE_ATTRIBUTE_KEY, &IGNORE_ATTRIBUTE_VALUE_IGNORED)
+    }
+}
+
+#[cfg(windows)]
+fn xattr_get(path: &Path, attribute_name: &str) -> Option<Vec<u8>> {
+    unsafe {
+        let stream_path = format!("{}:{}", path.to_str().unwrap(), attribute_name);
+        let winapi_path = U16CString::from_str_unchecked(&stream_path);
+
+        let handle = CreateFile2(
+            winapi_path.as_ptr(),
+            GENERIC_READ,
+            FILE_SHARE_READ,
+            OPEN_EXISTING,
+            ptr::null_mut(),
+        );
+
+        let error = GetLastError();
+        if error != 0 {
+            return None;
+        }
+
+        let mut data = [0u8; 2];
+        let mut len = 0;
+        let error = ReadFile(
+            handle,
+            data.as_mut_ptr() as LPVOID,
+            data.len() as u32,
+            &mut len,
+            ptr::null_mut(),
+        );
+
+        if error != TRUE {
+            let last_error = GetLastError();
+            warn!(
+                "Failed reading stream data from {:?} with {}",
+                stream_path, last_error
+            );
+            CloseHandle(handle);
+            return None;
+        }
+
+        CloseHandle(handle);
+        let mut attribute_data = data.to_vec();
+        attribute_data.truncate(len as usize);
+
+        Some(attribute_data)
+    }
+}
+
+#[cfg(windows)]
+fn xattr_set(path: &Path, attribute_name: &str, attribute_value: &[u8; 1]) -> bool {
+    unsafe {
+        let stream_path = format!("{}:{}", path.to_str().unwrap(), attribute_name);
+        let winapi_path = U16CString::from_str_unchecked(&stream_path);
+
+        let handle = CreateFile2(
+            winapi_path.as_ptr(),
+            GENERIC_WRITE,
+            FILE_SHARE_WRITE,
+            CREATE_ALWAYS,
+            ptr::null_mut(),
+        );
+
+        let last_error = GetLastError();
+        if last_error != 0 {
+            warn!(
+                "Failed writing stream data to {:?} with {}",
+                stream_path, last_error
+            );
+            return false;
+        }
+
+        let mut bytes_written = 0;
+        let success = WriteFile(
+            handle,
+            attribute_value.as_ptr() as LPCVOID,
+            attribute_value.len() as DWORD,
+            &mut bytes_written,
+            ptr::null_mut(),
+        );
+
+        if success == FALSE {
+            let last_error = GetLastError();
+            warn!(
+                "Failed writing stream data to {:?} with {}",
+                stream_path, last_error
+            );
+            CloseHandle(handle);
+            return false;
+        }
+
+        CloseHandle(handle);
+        true
     }
 }
 
@@ -47,7 +178,10 @@ mod tests {
     use tempfile::{Builder, NamedTempFile, TempDir};
 
     const TEST_ITEM_PREFIX: &str = "dropignore-testing";
+    #[cfg(unix)]
     const TEST_IGNORE_ATTRIBUTE_KEY: &str = "user.com.dropbox.ignored";
+    #[cfg(windows)]
+    const TEST_IGNORE_ATTRIBUTE_KEY: &str = "com.dropbox.ignored";
     const TEST_IGNORE_ATTRIBUTE_VALUE_IGNORED: [u8; 1] = [b'1'];
 
     #[rstest(arrange_ignored, case::not_ignored(false), case::ignored(true))]
@@ -142,6 +276,7 @@ mod tests {
         cache_dir.into()
     }
 
+    #[cfg(unix)]
     fn arrange_ignored_attribute(path: &Path) {
         xattr::set(
             path,
@@ -149,5 +284,14 @@ mod tests {
             &TEST_IGNORE_ATTRIBUTE_VALUE_IGNORED,
         )
         .unwrap();
+    }
+
+    #[cfg(windows)]
+    fn arrange_ignored_attribute(path: &Path) {
+        super::xattr_set(
+            path,
+            TEST_IGNORE_ATTRIBUTE_KEY,
+            &TEST_IGNORE_ATTRIBUTE_VALUE_IGNORED,
+        );
     }
 }
